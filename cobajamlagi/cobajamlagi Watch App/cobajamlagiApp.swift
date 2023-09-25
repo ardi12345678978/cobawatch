ad324a3647f446ff869c5536f3b02d34f744a3f5
ardi12345678978 <benedictusardi1@gmail.com>
2023-09-25 17:23:07 +0700

    p

---
+++ cobajamlagi/cobajamlagi Watch App/cobajamlagiApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobajamlagiApp.swift
+//  cobajamlagi Watch App
+//
+//  Created by Benedictus Harkristanto Ardi on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct cobajamlagi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
